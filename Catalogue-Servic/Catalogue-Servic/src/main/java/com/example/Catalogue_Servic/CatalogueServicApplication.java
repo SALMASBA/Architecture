a2baa476167ab7ccb183b44cb282d5979c4ac8aa
SALMASBA <salmasbaa550@gmail.com>
2025-12-08 14:47:06 +0100

    first commit

---
+++ Catalogue-Servic/Catalogue-Servic/src/main/java/com/example/Catalogue_Servic/CatalogueServicApplication.java
@@ -0,0 +1,13 @@
+package com.example.Catalogue_Servic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueServicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueServicApplication.class, args);
+	}
+
+}
---
+++ Catalogue-Servic/Catalogue-Servic/src/test/java/com/example/Catalogue_Servic/CatalogueServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Catalogue_Servic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueServicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
